7ce84d0e3115bb4cff490f8cbd151e5f32a053d9
MauroGallo <fmgallo@personalsoft.com.co>
2023-08-11 17:30:09 -0500

    adicion propiedades mapas

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,4 +1,3 @@
-const { PoolCluster } = require("mysql2/typings/mysql/lib/PoolCluster");
 
 (function() {
 
@@ -7,7 +6,9 @@ const { PoolCluster } = require("mysql2/typings/mysql/lib/PoolCluster");
     const lng = -75.5997826;
     const mapa = L.map('mapa').setView([lat, lng ], 16);
     let marker;
-    
+
+    // utilizar provider y geocoder
+    const gocodeService =L.esri.Geocoding.geocodeService();
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -23,8 +24,22 @@ const { PoolCluster } = require("mysql2/typings/mysql/lib/PoolCluster");
     //detectar el moviemiento de pin
     marker.on('moveend', function(e){
         marker = e.target        
-        const posicion = marker.getLatLng();        
+        const posicion = marker.getLatLng();     
+        
+        console.log(posicion)
         mapa.panTo(new L.LatLng(posicion.lat,posicion.lng))
+
+        // obtener informacion de la calle a l soltar el pin
+        gocodeService.reverse().latlng(posicion,13).run(function(error, resultado){
+            //console.log(resultado)
+            marker.bindPopup(resultado.address.LongLabel)
+
+            // llenar los campos
+            document.querySelector('.calle').textContent = resultado?.address?.Address ?? '';
+            document.querySelector('#calle').value = resultado?.address?.Address ?? '';
+            document.querySelector('#lat').value = resultado?.latlng?.lat ?? '';
+            document.querySelector('#lng').value = resultado?.latlng?.lng ?? '';
+        })
     })
 
 })()
