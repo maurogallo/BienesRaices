f21d6f0c573799d155861df2a530d58bd4e4f41a
MauroGallo <fmgallo@personalsoft.com.co>
2023-08-14 22:53:17 -0500

    creando validaciones en el router

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -1,31 +1,53 @@
-import Precio from "../models/Precio.js"
-import Categoria from "../models/Categoria.js"
+import { validationResult } from "express-validator";
+import Precio from "../models/Precio.js";
+import Categoria from "../models/Categoria.js";
 
-const admin = (req,res) => {
-    res.render('propiedades/admin',{
-        pagina: 'Mis Propiedades',
-        barra: true
-    })
-}
 
-const crear = async (req,res) => {
+const admin = (req, res) => {
+  res.render("propiedades/admin", {
+    pagina: "Mis Propiedades",
+    barra: true,
+  });
+};
+
+const crear = async (req, res) => {
+  //Consultar modelo de Precio y categoria
+
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll(),
+  ]);
+
+  res.render("propiedades/crear", {
+    pagina: "Crear Propiedad",
+    barra: true,
+    csrfToken: req.csrfToken(),
+    precios,
+    categorias
+  });
+};
+
+const guardar = async (req, res) => {
+  // Validación
+  let resultado = validationResult(req);
+
+  if (!resultado.isEmpty()) {
     //Consultar modelo de Precio y categoria
 
     const [categorias, precios] = await Promise.all([
-        Categoria.findAll(),
-        Precio.findAll()
-    ])
+      Categoria.findAll(),
+      Precio.findAll(),
+    ]);
 
-    res.render('propiedades/crear',{
-        pagina: 'Crear Propiedad',
-        barra: true,
-        precios,
-        categorias,
-        
-    })
-}
+    return res.render("propiedades/crear", {
+      pagina: "Crear Propiedad",
+      barra: true,
+      csrfToken: req.csrfToken(),
+      precios,
+      categorias,
+      errores: resultado.array(),
+    });
+  }
+};
 
-export {
-    admin,
-    crear
-}
+export { admin, crear, guardar };
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { admin, crear } from '../controllers/propiedadesController.js'
+import { body } from 'express-validator'
+import { admin, crear, guardar } from '../controllers/propiedadesController.js'
 
 const router = express.Router();
 
 router.get('/mis-propiedades', admin)
 router.get('/propiedades/crear', crear)
+router.post('/propiedades/crear', 
+body('titulo').notEmpty().withMessage('El Titulo del Anuncio es Obligatorio'),
+guardar)
 
 
 export default router
